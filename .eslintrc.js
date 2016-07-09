9cd1b701a02d5d0dc6b063d9243ad4e7bd22439d
sisisin <azsisisin@gmail.com>
2016-07-09 09:39:59 +0900

    setup front

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,7 +5,10 @@ module.exports = {
         "es6": true,
         "node": true
     },
-    "extends": "eslint:recommended",
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
     "parserOptions": {
         "ecmaFeatures": {
             "experimentalObjectRestSpread": true,
@@ -33,5 +36,11 @@ module.exports = {
             "always"
         ],
         "no-unused-vars": ["error", { "args": "none" }]
+    },
+    "settings": {
+        "react": {
+            "pragma": "React",
+            "version": "15.2.1"
+        }
     }
 };
