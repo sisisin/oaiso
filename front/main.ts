5e7d738b8507aef027f222c7930819251b887253
sisisin <azsisisin@gmail.com>
2016-12-17 23:19:14 +0900

    feat(front) setup development env

--- front/main.ts
+++ front/main.ts
@@ -1,12 +1,8 @@
-import './polyfills.ts';
+import './polyfills';
 
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment';
-import { AppModule } from './app/';
+// import { enableProdMode } from '@angular/core';
+import { AppModule } from './app/app.module';
 
-if (environment.production) {
-  enableProdMode();
-}
 
 platformBrowserDynamic().bootstrapModule(AppModule);
---
+++ webpack.config.dev.js
@@ -0,0 +1,21 @@
+const path = require("path");
+
+module.exports = {
+  entry: {
+    "index": path.resolve(__dirname, "./_tmp/main.js"),
+  },
+  output: {
+    path: path.join(__dirname, "./server/public/js"),
+    filename: "[name].js"
+  },
+  resolve: {
+    extensions: ['.ts', '.js']
+  },
+  module: {
+    loaders: [
+      { test: /\.js$/, loaders: ['angular2-template-loader'], exclude: /node_modules/ },
+      { test: /\.component\.html$/, loader: ["raw-loader"] },
+      { test: /\.component\.css$/, loader: ["raw-loader"] },
+    ]
+  }
+};
