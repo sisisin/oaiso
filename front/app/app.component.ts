f8de2ce9ada1ca99b66feaa4d9aad848c7c6679f
sisisin <azsisisin@gmail.com>
2016-12-27 23:58:33 +0900

    feat(front) close side-nav when routing started

--- front/app/app.component.ts
+++ front/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationStart } from '@angular/router';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +8,16 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
+  constructor(private router: Router) { }
   ngOnInit() {
     $(".button-collapse").sideNav({ draggable: true });
+    this.router.events
+      .filter(ev => ev instanceof NavigationStart)
+      .subscribe(() => {
+        setTimeout(() => {
+          $('.button-collapse').sideNav('hide');
+        }, 150);
+      });
+
   }
 }
