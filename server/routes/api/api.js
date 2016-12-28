eab2be1cd9add401851832dedba2d9bd309963ec
sisisin <azsisisin@gmail.com>
2016-12-28 18:16:10 +0900

    fix&feat(front) fix api param check and mockup /copy/post

--- server/routes/api/api.js
+++ server/routes/api/api.js
@@ -9,17 +9,19 @@ api.post('/update', update.post);
 
 api.get('/profile', profile.get);
 
-api.param('id', circle.param);
+api.param('circle_id', circle.param);
 api.get('/circle', circle.get);
 api.post('/circle', circle.post);
-api.put('/circle/:id', circle.put);
+api.put('/circle/:circle_id', circle.put);
 
-api.param('id', event.param);
+api.param('event_id', event.param);
 api.get('/event', event.list);
 api.post('/event', event.post);
-api.put('/event/:id', event.put);
-api.delete('/event/:id', event.delete);
+api.put('/event/:event_id', event.put);
+api.delete('/event/:event_id', event.delete);
 
-api.put('/copy', copy.put);
+api.param('copy_id', copy.param);
+api.post('/copy', copy.post);
+api.put('/copy/:copy_id', copy.put);
 
 module.exports = { api };
--- server/routes/api/copy.js
+++ server/routes/api/copy.js
@@ -1,9 +1,19 @@
+const param = (req, res, next, id) => {
+  req.id = id;  // todo findbydb
+  next();
+};
+
 const put = (req, res) => {
+  const {title, circulation, price} = req.body;
+  res.json({ id: req.id, title, circulation, price });
+};
+
+const post = (req, res) => {
   const {title, circulation, price} = req.body;
   if (title == null || title === '') return res.json(null);
   if (circulation == null || circulation === '') return res.json(null);
   if (price == null || price === '') return res.json(null);
-  res.json({id:42, title, circulation, price});
+  res.json({ id: 42, title, circulation, price });
 };
 
-module.exports = { put };
+module.exports = { param, post, put };
