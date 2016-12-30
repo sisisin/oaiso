057196a59c93840a4e2e6bd8d67839915733fac7
sisisin <azsisisin@gmail.com>
2016-12-30 14:40:13 +0900

    fix(server) set redis expire

--- server/config/app.js
+++ server/config/app.js
@@ -4,9 +4,11 @@ const RedisOption = isPrd ? {
   port: process.env.REDIS_PORT
   , host: process.env.REDIS_HOST
   , pass: process.env.REDIS_PW
+  , ttl: 259200 // sec. 3day expire  
 } : {
     port: 6379
     , host: 'localhost'
+  , ttl: 86400 // sec. 1day expire  
   };
 
 const PassportOption = {
